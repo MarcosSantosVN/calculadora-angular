d7ecb0bc36ef1a0208b42ec97ff9687d98b6c03f
Marcos Santos <marcos.santos@viewnext.com>
2024-05-08 11:14:46 +0200

    Primer commit con el modulo de calculadora, la pagina principal y las carpetas creadas

---
+++ src/app/calculadora/services/calculadora.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MainPageComponent } from '../pages/main-page.component';
+
+
+@NgModule({
+
+  declarations: [
+    MainPageComponent
+  ],
+  exports: [
+    MainPageComponent
+  ]
+})
+export class CalculadoraModule { }
