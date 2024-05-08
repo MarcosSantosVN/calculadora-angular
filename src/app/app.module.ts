142272d892bb454e4101917f9146ce9acfec1c23
Marcos Santos <marcos.santos@viewnext.com>
2024-05-08 11:34:47 +0200

    Html de los componentes cuenta e historial funcionales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculadoraModule } from './calculadora/services/calculadora.module';
+import { HistorialComponent } from './calculadora/components/historial/historial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/calculadora/services/calculadora.module.ts
+++ src/app/calculadora/services/calculadora.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { MainPageComponent } from '../pages/main-page.component';
+import { CuentaComponent } from '../components/cuenta/cuenta.component';
+import { HistorialComponent } from '../components/historial/historial.component';
 
 
 @NgModule({
 
   declarations: [
+    CuentaComponent,
+    HistorialComponent,
     MainPageComponent
   ],
   exports: [
+    CuentaComponent,
+    HistorialComponent,
     MainPageComponent
   ]
 })
