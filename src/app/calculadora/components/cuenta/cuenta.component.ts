e52c8dcd8fddc76ce98f3f247562695808078238
Marcos Santos <marcos.santos@viewnext.com>
2024-05-08 12:27:58 +0200

    Funcionalidad de la calculadora terminada

--- src/app/calculadora/components/cuenta/cuenta.component.ts
+++ src/app/calculadora/components/cuenta/cuenta.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-calc-cuenta',
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrl: './cuenta.component.css'
 })
 export class CuentaComponent {
+
+  @Output()
+  public onNewResult: EventEmitter<String> = new EventEmitter();
+
+  public cuenta: string = '';
+  public flag: boolean = false;
+
+  escribir(value: string, simbol:boolean): void {
+    if (!simbol && this.flag) {
+      this.cuenta = value;
+    } else {
+      this.cuenta += value;
+    }
+    this.flag = false;
+  }
+
+  limpiar(): void {
+    this.cuenta = '';
+  }
+
+  borrar(): void {
+    if (this.cuenta.length == 1 ||  this.flag) {
+      this.cuenta = '';
+      this.flag = false;
+    } else {
+      this.cuenta = this.cuenta.slice(0, -1);
+    }
+  }
+
+  calcular(): void {
+    try {
+      this.cuenta = eval(this.cuenta);
+
+    } catch {
+      this.cuenta = "Error";
+    }
+    this.flag = true;
+
+    this.onNewResult.emit(this.cuenta);
+
+  }
 
 }
